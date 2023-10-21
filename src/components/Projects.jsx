76b1dd4a1c3827f9bbb04e14206419262c645d6b
Krieger28 <ivankoch28@gmail.com>
2023-10-21 01:52:20 -0300

    img and links update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,10 +3,9 @@ import { BsYoutube, BsGithub } from "react-icons/bs";
 import css from "./css/projects.module.css";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
-
-// import fotoftofot from "./img/nombredefotooo"
-// import fotoftofot from "./img/nombredefotooo"
-// import fotoftofot from "./img/nombredefotooo"
+import pkdxpng from "./img/pkdxpng.png"
+import notespng from "./img/NOTESPNG.png"
+import wallabypng from "./img/wallabypng.png"
 
 function Projects() {
   return (
@@ -16,14 +15,14 @@ function Projects() {
           <h1>My main Proyects</h1>
           <div className={css.cards}>
             <div className={css.card}>
-              <img className={css.img} src="fotooo" alt="" />
+              <img className={css.img} src={wallabypng} alt="altt" />
               <div className={css.text}>
-                <h1>nombree</h1>
+                <h1>NFT Marketplace</h1>
                 <div className={css.link}>
-                  <a className={css.git_btn} href="githubbb">
+                  <a className={css.git_btn} href="https://github.com/Krieger28/WallabyApp-Frontend">
                     <BsGithub />
                   </a>
-                  <a className={css.yt_btn} href="youtbutee">
+                  <a className={css.yt_btn} href="https://www.youtube.com/watch?v=goCWOetsbH0&ab_channel=MaximilianoGiraudo">
                     <BsYoutube />
                   </a>
                 </div>
@@ -31,14 +30,14 @@ function Projects() {
             </div>
 
             <div className={css.card}>
-              <img className={css.img} src="fotooo" alt="" />
+              <img className={css.img} src={pkdxpng} alt="aaa" />
               <div className={css.text}>
-                <h1>nombree</h1>
+                <h1>Pokedex App</h1>
                 <div className={css.link}>
-                  <a className={css.git_btn} href="githubbb">
+                  <a className={css.git_btn} href="https://github.com/Krieger28/PI-Pokemon">
                     <BsGithub />
                   </a>
-                  <a className={css.yt_btn} href="youtbutee">
+                  <a className={css.yt_btn} href="https://youtu.be/SPo36GL_3YU">
                     <BsYoutube />
                   </a>
                 </div>
@@ -46,14 +45,14 @@ function Projects() {
             </div>
 
             <div className={css.card}>
-              <img className={css.img} src="fotooo" alt="" />
+              <img className={css.img} src={notespng} alt="aaaa" />
               <div className={css.text}>
-                <h1>nombree</h1>
+                <h1>Notes App</h1>
                 <div className={css.link}>
-                  <a className={css.git_btn} href="githubbb">
+                  <a className={css.git_btn} href="https://github.com/Krieger28/NotesApp">
                     <BsGithub />
                   </a>
-                  <a className={css.yt_btn} href="youtbutee">
+                  <a className={css.yt_btn} href="https://youtu.be/3spXpE_Kt3g">
                     <BsYoutube />
                   </a>
                 </div>
